7002afa662114e23c9648f95013da0bddb6c2947
ritu <irengomes018@gmail.com>
2025-09-28 01:03:38 +0600

    little code check

--- src/components/Testimonial/TestimonialCard.jsx
+++ src/components/Testimonial/TestimonialCard.jsx
@@ -5,11 +5,11 @@ const TestimonialCard = ({ name, review, role, avatarPlaceholder }) => {
     <div className="card bg-white shadow-xl hover:shadow-2xl transition-shadow duration-300 border border-gray-200">
       <div className="card-body text-center">
         <div className="text-3xl mb-4 text-red-500">
-            <div className="avatar">
-                <div className="w-13">
+          <div className="avatar">
+            <div className="w-13">
                 <img src={avatarPlaceholder} />
             </div>
-  </div>
+          </div>
         </div>
         <p className="text-gray-600 italic">{review}</p>
         <div className="mt-6">
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,5 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
-  
 }
 

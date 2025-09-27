1d9f42d8713d40f085715689f9f08e10ab8302a8
ritu <irengomes018@gmail.com>
2025-09-28 00:54:11 +0600

    restructuring

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,6 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#1d4ed8", // Professional blue
-          secondary: "#6b7280", // Neutral gray
-          accent: "#22c55e", // Green
-          neutral: "#f3f4f6", // Light background
-          "base-100": "#ffffff", // White for cards
-          "base-200": "#f9fafb", // Light gray for sections
-          "base-300": "#d1d5db", // Darker gray for footer
-        },
-      },
-    ],
-  },
+  
 }
 
